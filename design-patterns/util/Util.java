e5b90a5245de3690cff3f1be3f5b014752d2e833
hauntarl <sameer.mungole@gmail.com>
2022-10-20 13:43:03 -0700

    create utility for console output

---
+++ design-patterns/util/Util.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Util {
+    private Util() {
+    }
+
+    public static String dashes(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append('-');
+        }
+        return sb.toString();
+    }
+}
