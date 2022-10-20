ddcaa10100cf48d78990f66c3ec113114a5cff4a
hauntarl <sameer.mungole@gmail.com>
2022-10-19 22:13:05 -0700

    update task list

--- design-patterns/Main.java
+++ design-patterns/Main.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         // TODO: Next steps
-        // * 1. Create skeletons for all design patterns
-        // * 2. Add implementions for the design patterns
-        // * 3. Combine all the design patterns
-        // * 4. Add proper documentation and traceability to the design patterns
+        // * 1. Add Login functionality
+        // * 2. Create skeletons for all design patterns
+        // * 3. Add implementions for the design patterns
+        // * 4. Combine all the design patterns
+        // * 5. Add proper documentation and traceability to the design patterns
     }
 }
