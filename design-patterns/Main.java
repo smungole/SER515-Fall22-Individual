825f85ad0c13e99b67d1b35a6c37bfbc05125954
hauntarl <sameer.mungole@gmail.com>
2022-10-20 13:51:32 -0700

    update the flow of execution

--- design-patterns/Main.java
+++ design-patterns/Main.java
@@ -6,6 +6,7 @@ import menu.ProductMenu;
 import person.Person;
 import product.UserProduct;
 import product.UserProductDB;
+import util.Util;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,13 +17,17 @@ public class Main {
         System.out.println();
         Person person = Login.login();
 
-        // * 3. Show product type selection menu based on the type of person
-        person.showMenu();
+        while (true) {
+            // * 3. Show product type selection menu based on the type of person
+            person.showMenu();
 
-        // * 4. Show product options to the user
-        ProductMenu menu = person.createProductMenu();
-        UserProduct userProduct = new UserProduct(person, menu);
-        userProduct.showMenu();
+            // * 4. Show product options to the user
+            ProductMenu menu = person.createProductMenu();
+            UserProduct userProduct = new UserProduct(person, menu);
+            userProduct.showMenu();
+
+            showEOL(person.getName().length());
+        }
     }
 
     /**
@@ -35,5 +40,13 @@ public class Main {
         Login.loadSellers();
         ProductMenuDB.loadProducts();
         UserProductDB.loadUserProducts();
+    }
+
+    private static void showEOL(int size) {
+        System.out.printf(
+                "\n\t%s x %s x %s\n",
+                Util.dashes(20),
+                Util.dashes(11 + size),
+                Util.dashes(20));
     }
 }
