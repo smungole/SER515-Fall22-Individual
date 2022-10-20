73871acca1188db9f180e2cd39458d6395fab245
hauntarl <sameer.mungole@gmail.com>
2022-10-20 11:35:18 -0700

    implement filter products

--- design-patterns/menu/ProduceProductMenu.java
+++ design-patterns/menu/ProduceProductMenu.java
@@ -1,5 +1,6 @@
 package menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProduceProductMenu implements ProductMenu {
@@ -17,6 +18,17 @@ public class ProduceProductMenu implements ProductMenu {
             System.out.println(String.format("\t\t%d. %s", i + 1, products.get(i)));
         }
         System.out.println("\t---------- x ------------ x ----------");
+    }
+
+    @Override
+    public List<String> filterProducts(List<Integer> options) {
+        List<String> filtered = new ArrayList<>();
+        for (Integer option : options) {
+            if (option >= 0 && option < products.size()) {
+                filtered.add(products.get(option));
+            }
+        }
+        return filtered;
     }
 
 }
--- design-patterns/menu/ProductMenu.java
+++ design-patterns/menu/ProductMenu.java
@@ -1,5 +1,9 @@
 package menu;
 
+import java.util.List;
+
 public interface ProductMenu {
     public void showMenu();
+
+    public List<String> filterProducts(List<Integer> options);
 }
